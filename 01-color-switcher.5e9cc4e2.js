f261435fe63ebe555f85bceb3dcb2e588ad90247
GOOSxE <119361095+GOOSxE@users.noreply.github.com>
2023-05-06 20:08:00 +0000

    Deploying to gh-pages from @ GOOSxE/goit-js-hw-09@7bd8f855e8308e71b862a441c62ee30f2e3694e3 🚀

---
+++ 01-color-switcher.5e9cc4e2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");let o=!1;t.addEventListener("click",(()=>{o||(intervalId=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),o=!0)})),e.addEventListener("click",(function(){clearInterval(intervalId),o=!1})),t.classList.add("body__start-btn"),e.classList.add("body__stop-btn");
+//# sourceMappingURL=01-color-switcher.5e9cc4e2.js.map

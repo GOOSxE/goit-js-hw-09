4a9d15bd8f0f5f15d5268bfa2abec16b557e53ad
GOOSxE <der.goose.4308@gmail.com>
2023-05-06 21:38:23 +0200

    completed first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+// *
+const startButtonUrl = document.querySelector('button[data-start]');
+const stopButtonUrl = document.querySelector('button[data-stop]');
+const bodyUrl = document.querySelector('body');
+// ? Посилання
 
+let isActive = false;
+startButtonUrl.addEventListener('click', () => {
+  if (!isActive) {
+    intervalId = setInterval(() => {
+      bodyUrl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    isActive = true;
+  }
+});
+
+stopButtonUrl.addEventListener('click', () => {
+  clearInterval(intervalId);
+  isActive = false;
+});
+
+// todo // Стилі кнопок
+startButtonUrl.classList.add('body__start-btn');
+stopButtonUrl.classList.add('body__stop-btn');
+// * // Отримання кольору
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
